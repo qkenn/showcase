72b6586dcfaaf0946f21b1da2b6f12c19e3c6119
qkenn <github.com.sampling990@passinbox.com>
2024-08-12 11:03:27 +0530

    build: add html-loader for images referenced in html in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ const commonConfig = {
         },
       },
       {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
